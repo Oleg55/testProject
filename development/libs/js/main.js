26cc4b6bcabaae996ce025e4a89d01e152f9ee4e
Oleg <Shevchenko.oleg.1992@gmail.com>
2019-11-07 00:02:19 +0200

    Commit

--- development/libs/js/main.js
+++ development/libs/js/main.js
@@ -6,3 +6,43 @@ $('.header_catalog').click(function(){
 	$('.header_catalog-dropdown').fadeToggle();
 	$('.header_catalog-img').toggleClass('rotated');
 });
+
+$('.slider-main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    swipe: true,
+    dots: true,
+    adaptiveHeight: true,
+    dotsClass: 'slider-main-dots'
+});
+
+$('.brands-slider').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: true,
+    adaptiveHeight: true,
+   	infinite: false,
+    prevArrow: $('.brands-slider-prev'),
+    nextArrow: $('.brands-slider-next'),
+    responsive: [
+        {
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+    ]
+});
